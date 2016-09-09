c0447210a21d13743159f09ec07d59f180abfdcd
j7sx <j7sx@users.noreply.github.com>
2016-09-09 11:35:22 +0000

    add some row

--- db_create.py
+++ db_create.py
@@ -6,6 +6,7 @@ import sqlite3
 db = sqlite3.connect("site.db")
 cur = db.cursor()
 cur.execute("create table if not exists users(id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, \
-login varchar(100), password varchar(100), email varchar(100), last_ip varchar(100))")
+login varchar(100), password varchar(100), email varchar(100), session_id varchar(100), \
+admin int(1), last_ip varchar(100), last_login varchar(100))")
 db.commit()
 db.close()
